1092bd6ab4e8082445643b375a3143a8bbf34a20
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-24 08:00:34 +0000

    mise en place de la structure exercice DOM

---
+++ exercice DOM/index.js
@@ -0,0 +1,42 @@
+function exercice1()
+{
+
+}
+
+function exercice2()
+{
+
+}
+
+function exercice3()
+{
+    let ingredients = ["Salade", "Tomate", "Oignon"];
+}
+
+function exercice4()
+{
+    let heroes = [
+        {
+            name : "Clark Kent",
+            secret : "Superman"
+        },
+        {
+            name : "Diana Prince",
+            secret : "Wonder Woman"
+        },
+        {
+            name : "Tony Stark",
+            secret: "Iron Man"
+        }
+    ];
+}
+
+function exercice5()
+{
+
+}
+
+function exercice6()
+{
+
+}
