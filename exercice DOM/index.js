44db4cd5a038eb7cc1d2862398574274ff8c14cc
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-24 10:09:41 +0000

    mise en place de la structure exercice DOM V.2

--- exercice DOM/index.js
+++ exercice DOM/index.js
@@ -22,7 +22,7 @@ function exercice2()
 function exercice3()
 {
     let ingredients = ["Salade", "Tomate", "Oignon"];
-    let ul = document.querySelector("#ex3>ul");
+    let ul = document.querySelector("#ex3 > ul");
     let btnAddIngredients = document.getElementById("btn-add-ingredients");
     let li = document.createElement("li");
     let li1 = document.createElement("li");
@@ -58,21 +58,56 @@ function exercice4()
             secret: "Iron Man"
         }
     ];
+    
+    let btnAddHero = document.getElementById("btn-add-hero");
+    
+    var currentHeroIndex = 0;
+
+        function afficherHeroSuivant() {
+            var tableBody = document.querySelector("#ex4 > table > tbody");
+            if (currentHeroIndex < heroes.length) {
+                var hero = heroes[currentHeroIndex];
+                var row = document.createElement("tr");
+                var nameCell = document.createElement("td");
+                var secretCell = document.createElement("td");
+                nameCell.textContent = hero.name;
+                secretCell.textContent = hero.secret;
+                row.appendChild(nameCell);
+                row.appendChild(secretCell);
+                tableBody.appendChild(row);
+                currentHeroIndex++;
+            }
+        }
+        
+        btnAddHero.addEventListener("click", function(){
+            afficherHeroSuivant();
+        });
 }
 
 function exercice5()
 {
+    let boxBlue = document.getElementById("blue");
+    let boxRed = document.getElementById("red");
+    let boxGreen = document.getElementById("green");
+    let btnTurn = document.getElementById("btn-turn");
+    
+    btnTurn.addEventListener("click", function(event){
+        boxBlue.style.backgroundColor = "red";
+        boxRed.style.backgroundColor = "green";
+        boxGreen.style.backgroundColor = "blue";
+    });
 
 }
 
 function exercice6()
 {
-
 }
 
 window.addEventListener("DOMContentLoaded", function(){
     exercice1();
     exercice2();
     exercice3();
+    exercice4();
+    exercice5();
 });
 
