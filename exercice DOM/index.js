f442c3e8f4b6bf6b4241281389401df074bb7853
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-24 08:58:20 +0000

    mise en place de la structure exercice DOM V.1

--- exercice DOM/index.js
+++ exercice DOM/index.js
@@ -1,16 +1,45 @@
 function exercice1()
 {
-
+    let box1 = document.getElementById("box1");
+    let btnChangeColor = document.getElementById("btn-change-color");
+    
+    btnChangeColor.addEventListener("click", function(event){
+        console.log('je clique');
+        box1.style.backgroundColor = "blue";
+    });
 }
 
 function exercice2()
 {
-
+    let box2 = document.getElementById("box2");
+    let btnMove = document.getElementById("btn-move");
+    
+    btnMove.addEventListener("click", function(event){
+       box2.classList.toggle('move');
+    });
 }
 
 function exercice3()
 {
     let ingredients = ["Salade", "Tomate", "Oignon"];
+    let ul = document.querySelector("#ex3>ul");
+    let btnAddIngredients = document.getElementById("btn-add-ingredients");
+    let li = document.createElement("li");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let txt = document.createTextNode("Salade");
+    let txt1 = document.createTextNode("Tomate");
+    let txt2 = document.createTextNode("Oignon");
+    
+    btnAddIngredients.addEventListener("click", function(event){
+      ul.appendChild(li);
+      ul.appendChild(li1);
+      ul.appendChild(li2);
+      li.appendChild(txt);
+      li1.appendChild(txt1);
+      li2.appendChild(txt2);
+    });
+    
 }
 
 function exercice4()
@@ -40,3 +69,10 @@ function exercice6()
 {
 
 }
+
+window.addEventListener("DOMContentLoaded", function(){
+    exercice1();
+    exercice2();
+    exercice3();
+});
+
